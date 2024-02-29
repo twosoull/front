a7aae790b9863448c8ff5d73917e2ee1763a4fa6
iyeonghun <twosoull@naver.com>
2024-02-29 19:21:58 +0900

    react + spring axios 연결 완료 cors 처리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  axios.get("/api/main").then((result)=>{
+    console.log(result.data);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
